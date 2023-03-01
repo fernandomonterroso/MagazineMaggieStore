0cd1a5371112046e07b43a1330e2e8b09fc07e8b
unknown <fmonterroso@combexim.com.gt>
2023-02-28 22:37:51 -0600

    first commit

---
+++ scripts.js
@@ -0,0 +1,57 @@
+var dataURL = 'https://jsonplaceholder.typicode.com/users';
+
+var jsonData = [
+    {
+        "nombre": "Calcetin",
+        "precio": "45.45",
+        "foto": "1.jpg"
+    },
+    {
+        "nombre": "Mochila",
+        "precio": "45.23",
+        "foto": "2.jpg"
+    },
+    {
+        "nombre": "Pan",
+        "precio": "12",
+        "foto": "3.jpg"
+    },
+    {
+        "nombre": "Condones",
+        "precio": "45",
+        "foto": "4.jpg"
+    },
+    {
+        "nombre": "Collar",
+        "precio": "500",
+        "foto": "5.jpg"
+    },
+    {
+        "nombre": "Cerdido en mole",
+        "precio": "25.2",
+        "foto": "6.jpg"
+    }
+];
+
+new Vue({
+  el: '#app',
+  data: {
+    items: null,
+	title: "Catalogo Maggie Store"
+  },
+  created: function () {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: function () {
+		var self = this;
+        self.items = jsonData;
+		/*
+        var total;
+		$.get(dataURL, function( data ) {
+			self.items = data;
+		});
+        */
+    }
+  }
+});
